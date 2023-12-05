b228a65358545a8262d687694e7dfef2826e0094
Viviana <vivianaherrerahe@gmail.com>
2023-12-05 13:37:30 -0500

    config cors

--- vite.config.js
+++ vite.config.js
@@ -3,26 +3,36 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
     },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://178.16.142.114', // Reemplaza con la dirección IP de tu servidor Laravel
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+    host: 'homiliasuriel.com', // Reemplaza con tu dominio
+  },
 });

f0710028460ee6f62718cc54ee2a9c02fdba17fd
Viviana <vivianaherrerahe@gmail.com>
2023-12-31 15:36:06 -0500

    se agrega descip sacerdote

--- resources/js/components/Priest.vue
+++ resources/js/components/Priest.vue
@@ -10,18 +10,17 @@
             dataPrayerDesc.link
             " frameborder="0" allowfullscreen></iframe>
         </div>
-        <div class="bg-white rounded-lg p-8 md:p-12">
+        <div class="bg-white rounded-lg p-2 md:p-1">
           <p class="mb-3 font-bold text-custom-text md:text-2xl">Sacerdote</p>
           <p class="mb-3 text-black font-semibold">
             Uriel Alejandro Franco Sánchez
           </p>
-          <p class="mb-3 text-gray-500">
-            So I started to walk into the water. I won't lie to you boys, I was
-            terrified. But I pressed on, and as I made my way past the breakers
-            a strange calm came over me. I don't know if it was divine
-            intervention or the kinship of all living things but I tell you
-            Jerry at that moment, I was a marine biologist.
+          <p id="texto" class="mb-3 text-gray-500">
+            {{ showFullText ? fullText : halfText }}
           </p>
+          <button @click="toggleText" class="text-blue-500 cursor-pointer">
+            {{ showFullText ? "Ver menos" : "Ver más" }}
+          </button>
         </div>
         <div class="md:flex md:flex-col md:items-center">
           <div class="relative bg-white rounded-lg sm:p-8 md:p-12">
@@ -38,6 +37,12 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import axios from "axios";
+
+const fullText = ref('Presbítero Magister - sacerdote de la Diócesis de Neiva, Colombia. Graduado de la Licenciatura en Matemáticas, Normalista de la Escuela Normal Superior de Neiva. Licenciado en Filosofía y Educación Religiosa de la Universidad Católica de Oriente en Rionegro – Antioquia. Teólogo de la misma universidad; Realizó sus estudios eclesiales en el seminario Cristo Sacerdote de la Ceja (Antioquia) en la asociación Siervos del Espíritu Santo – Magister en Asesoría Familiar y Gestión de proyectos de la Familia de la Universidad de la Sabana en Bogotá. Músico del conservatorio departamental del Huila y delegado Diocesano de la Pastoral Familiar y actualmente sacerdote de la Parroquia Sagrada Familia de Rivera.');
+
+const halfText = ref('');
+const showFullText = ref(false);
+
 const dataPrayerDesc = ref([]);
 const getPrayerDesc = async () => {
   const { data } = await axios.get('/prayers');
@@ -45,5 +50,10 @@ const getPrayerDesc = async () => {
 };
 onMounted(() => {
   getPrayerDesc();
+  const halfLength = Math.ceil(fullText.value.length / 2);
+  halfText.value = fullText.value.slice(0, halfLength)+'...';
 });
+const toggleText = () => {
+  showFullText.value = !showFullText.value;
+};
 </script>

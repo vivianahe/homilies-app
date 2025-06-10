3952d4d3c7382125e04145986118aeff9f034d18
VIVIANAHE19 <43278843+vivianahe@users.noreply.github.com>
2025-06-10 10:45:59 -0500

    Update HomilyView.vue

--- resources/js/components/views/HomilyView.vue
+++ resources/js/components/views/HomilyView.vue
@@ -5,6 +5,7 @@ import HomilyDay from "../HomilyDay.vue";
 import Priest from "../Priest.vue";
 import Footer from "../Footer.vue";
 import Reflection from "../Reflection.vue";
+import ReflectionWork from "../ReflectionWork.vue";
 import MessageTip from "../MessageTip.vue";
 import Contact from "../Contact.vue";
 import { ref, onMounted, onBeforeUnmount } from "vue";
@@ -40,6 +41,7 @@ onBeforeUnmount(() => {
   <carousel />
   <HomilyDay />
   <MessageTip />
+  <ReflectionWork />
   <Priest />
   <Reflection />
   <Contact id="contacto" />
